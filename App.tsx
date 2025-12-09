3033e035da48b4029fa3ff239a0a5a9102dc61f3
ALF4M <jose.ven.jim@gmail.com>
2025-12-09 05:11:45 +0100

    feat[30]: Adding navigation architecture from react native

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import HomeScreen from './src/screens/Home/HomeScreen';
+import DetailView from './src/screens/DetailView/DetailView';
+//import HistoryView from './src/screens/History/HistoryView';
 
-const App: React.FC = () => {
-  return <HomeScreen />;
+export default function App() {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailView} />
+        {/* <Stack.Screen name="History" component={HistoryView} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
-
-export default App;
--- src/screens/DetailView/DetailView.tsx
+++ src/screens/DetailView/DetailView.tsx
@@ -1,67 +1,62 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Alert, Image } from 'react-native'; 
-import { SafeAreaView } from 'react-native-safe-area-context'; 
-import { Ionicons } from '@expo/vector-icons';
 import * as Clipboard from 'expo-clipboard';
-import { styles } from './DetailView.styles'; 
-import { ScanRecord, DetailViewProps } from '../../types'; 
+import { styles } from './DetailView.styles';
+import { Ionicons } from '@expo/vector-icons';
+import { RootStackParamList } from '../../types';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { View, Text, TouchableOpacity, Alert, Image } from 'react-native';
+import { useNavigation, useRoute, RouteProp } from '@react-navigation/native';
+
 
 const getFormattedDateTime = (timestamp: number) => {
     const today = new Date(timestamp);
-    
-    const options: Intl.DateTimeFormatOptions = { 
-        day: '2-digit', 
-        month: 'short', 
+
+    const options: Intl.DateTimeFormatOptions = {
+        day: '2-digit',
+        month: 'short',
         year: 'numeric',
-        hour: '2-digit',   
-        minute: '2-digit', 
-        hour12: false 
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
     };
-    
+
     return today.toLocaleDateString(undefined, options);
 };
 
-export default function DetailView({ navigation, record }: DetailViewProps) {
-    const formattedDate = getFormattedDateTime(record.timestamp); 
+export default function DetailView() {
+    const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+
+    const route = useRoute<RouteProp<RootStackParamList, 'Detail'>>();
+    const record = route.params.record;
+
+    const formattedDate = getFormattedDateTime(record.timestamp);
 
     const copyToClipboard = async () => {
         await Clipboard.setStringAsync(record.recognizedNumber);
         Alert.alert("Éxito", "Número copiado al portapapeles");
     };
 
-    const handleDelete = () => {
-        console.log("Simulación de eliminación iniciada. Redirigiendo a Home.");
-        if (navigation) {
-            navigation.navigate('Home'); 
-        } else {
-            Alert.alert("Simulación", "Redirigiendo a Home View");
-        }
+    const handleDelete = async () => {
+        navigation.goBack()
     };
 
     return (
         <SafeAreaView style={styles.container}>
-            <View style={styles.header}>
-              <TouchableOpacity onPress={() => navigation?.goBack()}>
-                <Ionicons name="arrow-back" size={24} color="#333" />
-              </TouchableOpacity>
-              <Text style={styles.headerTitle}>Detalle</Text>
-              <View style={{ width: 24 }} /> 
-            </View>
-
             <View style={styles.content}>
                 <View style={styles.card}>
                     <Text style={styles.label}>Número detectado</Text>
                     <Text style={styles.detectedNumber}>{record.recognizedNumber}</Text>
-                    <Text style={styles.date}>{formattedDate}</Text> 
+                    <Text style={styles.date}>{formattedDate}</Text>
                 </View>
 
                 <View style={[styles.card, styles.imageCard]}>
-                    <Image 
-                       source={{ uri: record.imageUri }} 
-                        style={styles.detectedImage} 
+                    <Image
+                        source={{ uri: record.imageUri }}
+                        style={styles.detectedImage}
                     />
                 </View>
-            
+
                 <View style={styles.buttonContainer}>
                     <TouchableOpacity style={styles.buttonPrimary} onPress={copyToClipboard}>
                         <Ionicons name="copy-outline" size={20} color="#fff" style={{ marginRight: 8 }} />
